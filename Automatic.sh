b74991721ac840891ab0e425744a1ef32a306b70
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-02-27 11:02:46 -0300

    suprimo -e

--- Automatic.sh
+++ Automatic.sh
@@ -9,25 +9,25 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-echo -e "${GREEN}[+] Harvesting subdomains with assetfinder ++++++++++++++++++++++++++++++++${ENDCOLOR}"
+echo "${GREEN}[+] Harvesting subdomains with assetfinder ++++++++++++++++++++++++++++++++${ENDCOLOR}"
 assetfinder $url >> $url/recon/assets.txt
 cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-echo -e "${RED}subdomains found:${ENDCOLOR}"
+echo "${RED}subdomains found:${ENDCOLOR}"
 cat $url/recon/assets.txt | wc -l
 rm $url/recon/assets.txt
 
-echo -e "${GREEN}[+] Harvesting subdomains with amass ++++++++++++++++++++++++++++${ENDCOLOR}"
+echo "${GREEN}[+] Harvesting subdomains with amass ++++++++++++++++++++++++++++${ENDCOLOR}"
 amass enum -d $url >> $url/recon/s.txt
-echo -e "${RED}subdomains found:${ENDCOLOR}"
+echo "${RED}subdomains found:${ENDCOLOR}"
 cat $url/recon/s.txt | wc -l
 sort -u $url/recon/s.txt >> $url/recon/final.txt
 rm $url/recon/s.txt
 
-echo -e "${GREEN}[+] Lives subdomains...${ENDCOLOR}"
+echo "${GREEN}[+] Lives subdomains...${ENDCOLOR}"
 cat $url/recon/final.txt | httpx -silent >> $url/recon/lives.txt
 
-#echo -e "${GREEN}[+] Lives and more subdomains...${ENDCOLOR}"
+#echo "${GREEN}[+] Lives and more subdomains...${ENDCOLOR}"
 #cat $url/recon/final.txt | httpx --status-code --content-length -title -verbose  >> $url/recon/vivos.txt
 
-echo -e "${GREEN}[+] nuclei scan...${ENDCOLOR}"
+echo "${GREEN}[+] nuclei scan...${ENDCOLOR}"
 nuclei -l $url/recon/lives.txt -t cves >> $url/recon/nuclei.txt

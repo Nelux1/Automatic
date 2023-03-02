0aa60cf0b19254b1879ff46a07d887d682d02c40
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-03-02 15:12:01 -0300

    scans

--- Automatic.sh
+++ Automatic.sh
@@ -2,32 +2,49 @@
 
 url=$1
 
-mkdir $url 
-mkdir $url/recon 
+mkdir $url
 
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 echo -e "${GREEN}[+] Harvesting subdomains with assetfinder ++++++++++++++++++++++++++++++++${ENDCOLOR}"
-assetfinder $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+assetfinder $url >> $url/assets.txt
+cat $url/assets.txt | grep $1 >> $url/final.txt
 echo -e "${RED}subdomains found:${ENDCOLOR}"
-cat $url/recon/assets.txt | wc -l
-rm $url/recon/assets.txt
+cat $url/assets.txt | wc -l
+rm $url/assets.txt
+
+echo -e "${GREEN} Harvesting subdomains with subfinder +++++++++++++++++++++++++++${ENDCOLOR}"
+subfinder -d $url -silent >> $url/sub.txt
+cat $url/sub.txt | grep $1 | tee -a $url/final.txt
+echo -e "${RED}subdomains found:${ENDCOLOR}"
+cat $url/sub.txt | wc -l
+rm $url/sub.txt
 
 echo -e "${GREEN}[+] Harvesting subdomains with amass ++++++++++++++++++++++++++++${ENDCOLOR}"
-amass enum -d $url >> $url/recon/s.txt
+amass enum -d $url >> $url/amass.txt
+amass enum -active -d $url -brute -w ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt | tee -a $url/amass.txt
+cat $url/amass.txt | aquatone -ports xlarge -out $url/aqua_$url
+
 echo -e "${RED}subdomains found:${ENDCOLOR}"
-cat $url/recon/s.txt | wc -l
-sort -u $url/recon/s.txt >> $url/recon/final.txt
-rm $url/recon/s.txt
+cat $url/amass.txt | wc -l
+sort -u $url/amass.txt >> $url/final.txt
+rm $url/amass.txt
 
 echo -e "${GREEN}[+] Lives subdomains...${ENDCOLOR}"
-cat $url/recon/final.txt | httpx -silent >> $url/recon/lives.txt
+cat $url/final.txt | httpx -silent >> $url/lives.txt
 
-#echo -e "${GREEN}[+] Lives and more subdomains...${ENDCOLOR}"
-#cat $url/recon/final.txt | httpx --status-code --content-length -title -verbose  >> $url/recon/vivos.txt
+echo -e "${GREEN}[+] Lives and more subdomains...${ENDCOLOR}"
+cat $url/final.txt | httpx --status-code --content-length -title -verbose  >> $url/vivos.txt
 
 echo -e "${GREEN}[+] nuclei scan...${ENDCOLOR}"
-nuclei -l $url/recon/lives.txt -t cves >> $url/recon/nuclei.txt
+nuclei -l $url/lives.txt -t cves >> $url/nuclei.txt
+nuclei -l $url/lives.txt -t ~/nuclei-templates -es info | tee -a $url/nuclei.txt  
+nuclei -l $url/aqua_$url/aquatone_urls.txt -t ~/nuclei-templates -es info | tee -a $url/nuclei.txt 
+nuclei -l $url/aqua_$url -t cves | tee -a $url/nuclei.txt
+
+echo -e "${GREEN}[+] Scan xss...${ENDCOLOR}"
+cat $url/lives.txt | gau --subs | tee -a $url/archivo.txt | grep "=" | egrep -iv ".(jpg|peg|gif|css|tif|tiff|png|woff|woff2|ico|pdf|svg|txt|js)" | qsreplace '"><script>confirm(1)</script>'| tee -a $url/arch.json && cat $url/arch.json | while read host do; do curl --silent --path-as-is --insecure "$host" | grep -qs "<script>confirm(1)" && echo "$host \033[0;031mVulnerable\n" | tee -a $url/xss_vulnerables.txt;done
+rm $url/archivo.txt
+rm $url/arch.json

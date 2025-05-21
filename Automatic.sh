d6cf37b99d9be7a331435e2d751a9c35fdba63c0
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2025-05-20 23:43:30 -0300

    Update Automatic.sh

--- Automatic.sh
+++ Automatic.sh
@@ -51,6 +51,7 @@ fi
 scan_url() {
     local target=$1
     local folder="$output/$target"
+
     mkdir -p "$folder"
 
     echo -e "\e[32m[+] Enumerating subdomains with assetfinder...\e[0m"
@@ -73,10 +74,12 @@ scan_url() {
 
     echo -e "\e[32m[+] Checking lives subdomains with httpx...\e[0m"
     cat "$folder/final.txt" | httpx -p  80,81,300,443,591,593,832,981,1010,1311,2082,2087,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5800,6543,7000,7396,7474,8000,8001,8008,8014,8042,8069,8080,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9200,9443,9800,9981,12443,16080,18091,18092,20720,28017 > "$folder/live.txt"
-    cat "$folder/final.txt" | httpx --status-code --content-length -title -fr -verbose -p  80,81,300,443,591,593,832,981,1010,1311,2082,2087,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5800,6543,7000,7396,7474,8000,8001,8008,8014,8042,8069,8080,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9200,9443,9800,9981,12443,16080,18091,18092,20720,28017  >> "$folder/liveInfo.txt"
+
+    cat "$folder/final.txt" | httpx --status-code --content-length -title -fr -verbose -p  80,81,300,443,591,593,832,981,1010,1311,2082,2087,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5800,6543,7000,7396,7474,8000,8001,8008,8014,8042,8069,8080,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9200,9443,9800,9981,12443,16080,18091,18092,20720,28017 >> "$folder/liveInfo.txt"
+
     cat "$folder/live.txt" | httpx -ss -t 20 -system-chrome
-    mv output "$folder" 
-    rm  -r "$folder/final.txt"
+
+    rm -f "$folder/final.txt"
 
     if [[ "$mode" == "subs" ]]; then
         exit 0
@@ -93,12 +96,17 @@ scan_url() {
         echo -e "\e[32m[+] Running nuclei scans, xss and cors...\e[0m"
         nuclei -l "$folder/live.txt" -t ~/nuclei-templates -es info | tee -a "$folder/nuclei.txt"
 
-        cat "$folder/allurls.txt" | tee -a "$folder/archivo.txt" | grep "=" | egrep -iv ".(jpg|peg|gif|css|tif|tiff|png|woff|woff2|ico|pdf|svg|txt|js)" | qsreplace '"><script>confirm(1)</script>'| tee -a "$folder/arch.json" && cat "$folder/arch.json" | while read host do; do curl --silent --path-as-is --insecure "$host" | grep -qs "<script>confirm(1)" && echo "$host \033[0;031mVulnerable\n" | tee -a "$folder/xss_vulnerables.txt";done
-        rm  "$folder/archivo.txt"
-        rm  "$folder/arch.json"
-        cat "$folder/allurls.txt" | grep "=" | grep -v "^$" | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | grep -i "vulnerable" | tee -a "$folder/xss_vulnerables.txt"
-        cat "$folder/live.txt" | httpx -threads 300 -follow-redirects -silent | rush -j200 'curl -m5 -s -I -H "Origin:evil.com" {} | [[ $(grep -c "evil.com") -gt 0 ]] && printf "\n\033[0;32m[VUL TO CORS] - {}\e[m"' 2>/dev/null | tee -a "$folder/cors.txt"
+        cat "$folder/allurls.txt" | tee -a "$folder/archivo.txt" | grep "=" | \
+            egrep -iv ".(jpg|peg|gif|css|tif|tiff|png|woff|woff2|ico|pdf|svg|txt|js)" | \
+            qsreplace '"><script>confirm(1)</script>' | tee -a "$folder/arch.json" && \
+            cat "$folder/arch.json" | while read host; do curl --silent --path-as-is --insecure "$host" | grep -qs "<script>confirm(1)" && echo "$host \033[0;031mVulnerable\n" | tee -a "$folder/xss_vulnerables.txt"; done
 
+        rm -f "$folder/archivo.txt"
+        rm -f "$folder/arch.json"
+
+        cat "$folder/allurls.txt" | grep "=" | grep -v "^$" | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | grep -i "vulnerable" | tee -a "$folder/xss_vulnerables.txt"
+
+        cat "$folder/live.txt" | httpx -threads 300 -follow-redirects -silent | rush -j200 'curl -m5 -s -I -H "Origin:evil.com" {} | [[ $(grep -c "evil.com") -gt 0 ]] && printf "\n\033[0;32m[VUL TO CORS] - {}\e[m"' 2>/dev/null | tee -a "$folder/cors.txt"
     fi
 
     echo -e "\e[32m[+] Scan complete for $target. Results saved in $folder/\e[0m"
@@ -111,4 +119,3 @@ if [[ -n "$url_list" ]]; then
 else
     scan_url "$url"
 fi
-
